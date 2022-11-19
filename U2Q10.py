c6935be478a7fed4161ef1937af924dc671d320c
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2022-11-19 23:39:03 +0530

    U2Q10.py

---
+++ U2Q10.py
@@ -0,0 +1,15 @@
+"""
+Using for loop display number till 10.
+"""
+
+def rangeFor():
+    for i in range(0,10+1):
+        print(i)
+
+def listFor():
+    list1 = [0,1,2,3,4,5,6,7,8,9,10]
+    for i in list1:
+        print(i)
+
+rangeFor()
+listFor()
