c1dc99585ac20163c2b8dd269e4462a4e270f78a
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2022-11-20 00:17:20 +0530

    U3Q4.py

---
+++ U3Q4.py
@@ -0,0 +1,13 @@
+"""
+To display factorial of number 7.
+"""
+def factorial7():
+    sum = 1
+    for i in range(1, 7+1):
+        sum = sum * i # Just think over this, how this will is executed.
+    print(sum)
+
+factorial7()
+
+# Program works as expected.
+## Program is COMPLETE.
